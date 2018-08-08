5749e4b4c6e0a0e8d69d21675425fdb25f4d05e3
Srinivas K <srinivas.kasiriveni@emids.com>
2018-08-08 20:44:21 +0530

    Fixed the Auth configuration
    issue is fixed

--- src/app/constants/config.js
+++ src/app/constants/config.js
@@ -1,10 +1,10 @@
 //OIDC auth config for implicit mode
 export const AUTH_CONFIG_IMPLICIT = {
     clientId : "js",
-    authority :  "http://ch-oauth-api.coreoflowsandbox.com",//"http://localhost:5000", //"http://ch-oauth-api.coreoflowsandbox.com",
-    redirectUri : "http://ch-oauth-api.coreoflowsandbox.com:9009/#/loginCallBack/#",//"http://localhost:3000/#/loginCallBack/#",//"http://ch-oauth-api.coreoflowsandbox.com:9009/#/loginCallBack/#",
+    authority :  "http://ch-oauth-api.coreoflowsandbox.com",
+    redirectUri : "http://52.172.45.185:9009/#/loginCallBack/#",
     reponseType : "id_token token",
     scope : "openid profile api1",
-    postLogoutRedirectUri : "http://ch-oauth-api.coreoflowsandbox.com:9009/",//"http://localhost:3000/",//"http://ch-oauth-api.coreoflowsandbox.com:9009/",
+    postLogoutRedirectUri : "http://52.172.45.185:9009/#/",
     loadUserInfo : false
 };
