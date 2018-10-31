404ef763d8fccafab58c33ee835baf6801fad079
sonalisap <sonalisap@emids.com>
2018-10-31 18:21:31 +0530

    Asyncmsg image modal ui issue fixed

--- src/app/screens/AsyncMessage/Modals/ImageModal.js
+++ src/app/screens/AsyncMessage/Modals/ImageModal.js
@@ -6,8 +6,8 @@ export default class AsyncImgModalTemplate extends React.Component {
 
     render() {
         return (
-                <Modal isOpen={this.props.isOpen} backdrop="static" toggle={this.props.toggle} className={"MsgModal previewImgHolder" + this.props.className} centered={this.props.centered} >
-                    <ModalBody className="MsgModalBody previewImg">
+                <Modal isOpen={this.props.isOpen} backdrop="static" toggle={this.props.toggle} className={"MsgModal previewImgHolder " + this.props.className} centered={this.props.centered} >
+                    <ModalBody className="MsgModalBody">
                         <div className="block-picview">
                         <button type="button" className="close CloseMsgModal" data-dismiss="modal" aria-label="Close" onClick={this.props.toggle}>
                         <span aria-hidden="true">&times;</span>
