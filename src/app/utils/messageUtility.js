e7ba1db969b7818f111ebac06d0ca690ee43cbe6
Vimal Anbu <VimalA@emids.com>
2018-10-31 11:58:22 +0530

    Text Chnage in Msg

--- src/app/utils/messageUtility.js
+++ src/app/utils/messageUtility.js
@@ -1,5 +1,5 @@
 
 export const serviceRequestMessages = {
-    notInterestedServiceProvider: 'Are you sure you are Not Interested for the Service Request',
-    applyServiceProvider:'Are you sure you want to Apply for the Services',
+    notInterestedServiceProvider: 'Are you sure you are Not Interested for the Service Request ?',
+    applyServiceProvider:'Are you sure you want to Apply for the Services ?',
 }
