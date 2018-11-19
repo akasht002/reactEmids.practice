769336227d2603baa6fe8c77850887f302c3a7d6
PraveenaG <PraveenaG@PRAVEENAGAONKAR>
2018-11-19 11:23:49 +0530

    fixed bug

--- src/app/utils/messageUtility.js
+++ src/app/utils/messageUtility.js
@@ -2,4 +2,5 @@
 export const serviceRequestMessages = {
     notInterestedServiceProvider: 'Are you sure you are Not Interested for the Service Request ?',
     applyServiceProvider:'Do you want to Apply for this services request?',
+    cancelServiceProvider: 'Are you sure you want to Cancel the Service Request?'
 }
