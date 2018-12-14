19a7455b42b7b72ad85d00639d9dc564fcc339fb
rohitnagda <42795571+rohitnagda@users.noreply.github.com>
2018-12-14 23:28:34 +0530

    ui_fixes_dashboard

--- src/app/screens/Dashboard/serviceRequest.js
+++ src/app/screens/Dashboard/serviceRequest.js
@@ -113,7 +113,7 @@ class ServiceRequest extends React.Component {
     return (
       <div
         className={
-          this.state.showMore ? 'card ProfileCard' : 'card ProfileCard extended'
+          this.state.showMore ? 'card ProfileCard extended' : 'card ProfileCard'
         }
       >
         <div className='ProfileCardBody'>
