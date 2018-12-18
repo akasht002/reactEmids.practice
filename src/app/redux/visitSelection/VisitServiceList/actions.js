5c9911c5e392ce5f03e3d5701b107a76a21717e1
Akash Tirole <AkashT@emids.com>
2018-12-18 21:01:56 +0530

    fixed loading issue for sr

--- src/app/redux/visitSelection/VisitServiceList/actions.js
+++ src/app/redux/visitSelection/VisitServiceList/actions.js
@@ -1,12 +1,26 @@
 import { API } from '../../../services/api';
 import { elasticSearchGet, ServiceRequestGet } from '../../../services/http';
-import { startLoading, endLoading } from '../../loading/actions';
+// import { startLoading, endLoading } from '../../loading/actions';
 import { getUserInfo } from '../../../services/http';
 
 export const VisitServiceList = {
     getVisitServiceListSuccess: 'get_visit_service_list_success/visitservice',
     serviceRequestCount: 'serviceRequestCount/visitservice',
-    formDirtyVisitList: 'formDirtyVisitList/visitservice'
+    formDirtyVisitList: 'formDirtyVisitList/visitservice',
+    startLoading: 'startLoading/visitservice',
+    endLoading: 'endLoading/visitservice'
+};
+
+export const startLoading = () => {
+    return {
+      type: VisitServiceList.startLoading,
+    }
+  };
+  
+  export const endLoading = () => {
+    return {
+      type: VisitServiceList.endLoading,
+    }
 };
 
 export const getVisitServiceListSuccess = (data) => {
@@ -53,8 +67,8 @@ export function getServiceRequestCount () {
         .then(resp => {
           if (resp && resp.data) {
             dispatch(serviceRequestCountSuccess(resp.data))
+            dispatch(endLoading())
           }
-        dispatch(endLoading())
         })
         .catch(() => {
           dispatch(endLoading())
