568facb409e9b45deb1190afc5cd61efa130860d
Akash Tirole <AkashT@emids.com>
2018-12-27 13:53:51 +0530

    fixed service area in esp profile

--- src/app/screens/Profile/ServiceArea/Details.js
+++ src/app/screens/Profile/ServiceArea/Details.js
@@ -1,49 +1,52 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 export const Details = (props) => {
-    return (
-        props.ServiceAreaList && props.ServiceAreaList.map((serviceArea, i) => {
-            return (
-              <li className='SPAddressItems' key={serviceArea.addressId}>
-                <div className={'SPCertificateContent'}>
-                  <div className={'width100 d-flex'}>
-                    <span className={'AddressContentLabel'}>Street</span>
-                    <span className={'SPCertificateDesc'}>
-                      {serviceArea.streetAddress}
-                    </span>
-                  </div>
-                  <div className={'width100 d-flex'}>
-                    <span className={'AddressContentLabel'}>City</span>
-                    <span className={'SPCertificateDesc'}>{serviceArea.city}</span>
-                  </div>
-                  <div className={'width100 d-flex'}>
-                    <span className={'AddressContentLabel'}>State</span>
-                    <span className={'SPCertificateDesc'}>
-                      {serviceArea.stateName}
-                    </span>
-                  </div>
-                  <div className={'width100 d-flex'}>
-                    <span className={'AddressContentLabel'}>ZIP</span>
-                    <span className={'SPCertificateDesc'}>{serviceArea.zipCode}</span>
-                  </div>
-                  <div className={'width100 d-flex'}>
-                    <span className={'AddressContentLabel'}>Range</span>
-                    <span className={'SPCertificateDesc'}>{serviceArea.coverageArea}</span>
-                  </div>
-                </div>
-    
-                <i
-                  className='SPIconMedium SPIconDelete mr-3'
-                  id={serviceArea.addressId}
-                  onClick={e => props.showModalOnDelete(e)}
-                />
-                <i
-                  className='SPIconMedium SPIconEdit'
-                  id={serviceArea.addressId}
-                  onClick={e => props.editServiceArea(e)}
-                />
-              </li>
-            )
-          })
-    )
+  return (
+    props.ServiceAreaList && props.ServiceAreaList.map((serviceArea, i) => {
+      return (
+        <li className='SPAddressItems' key={serviceArea.addressId}>
+          <div className={'SPCertificateContent'}>
+            <div className={'width100 d-flex'}>
+              <span className={'AddressContentLabel'}>Street</span>
+              <span className={'SPCertificateDesc'}>
+                {serviceArea.streetAddress}
+              </span>
+            </div>
+            <div className={'width100 d-flex'}>
+              <span className={'AddressContentLabel'}>City</span>
+              <span className={'SPCertificateDesc'}>{serviceArea.city}</span>
+            </div>
+            <div className={'width100 d-flex'}>
+              <span className={'AddressContentLabel'}>State</span>
+              <span className={'SPCertificateDesc'}>
+                {serviceArea.stateName}
+              </span>
+            </div>
+            <div className={'width100 d-flex'}>
+              <span className={'AddressContentLabel'}>ZIP</span>
+              <span className={'SPCertificateDesc'}>{serviceArea.zipCode}</span>
+            </div>
+            <div className={'width100 d-flex'}>
+              <span className={'AddressContentLabel'}>Range</span>
+              <span className={'SPCertificateDesc'}>{serviceArea.coverageArea}</span>
+            </div>
+          </div>
+          {
+            props.isUser ? <Fragment>
+              <i
+                className='SPIconMedium SPIconDelete mr-3'
+                id={serviceArea.addressId}
+                onClick={e => props.showModalOnDelete(e)}
+              />
+              <i
+                className='SPIconMedium SPIconEdit'
+                id={serviceArea.addressId}
+                onClick={e => props.editServiceArea(e)}
+              />
+            </Fragment> : ''
+          }
+        </li>
+      )
+    })
+  )
 }
