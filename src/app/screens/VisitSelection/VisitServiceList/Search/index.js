21e9f997c9fa7b0cc07422a664fb9984e98bcc99
Akash Tirole <AkashT@emids.com>
2019-02-12 18:50:18 +0530

    Fixed In verify my mobile number screen--Support hyper link is not working
    implemented search in service requests

--- src/app/screens/VisitSelection/VisitServiceList/Search/index.js
+++ src/app/screens/VisitSelection/VisitServiceList/Search/index.js
@@ -5,14 +5,14 @@ function Search(props){
         <Fragment>
         <span className='ProfileIconSearch' onClick={props.toggleSearch}/>
         <div className={'ProfileSearchContainer form-group ' + props.searchOpen}>
-            <div>
+        <form onSubmit={(e)=>props.handleSearchData(e)}>
                 <input className='form-control' type='text' value={props.searchKeyword} 
-                 onChange={(e)=>props.handleSearchkeyword(e)} placeholder='Type here to Search'  onKeyPress={(e) =>props.handleSearchkeywordPress(e)}
+                 onChange={(e)=>props.handleSearchkeyword(e)} placeholder='Type here to Search'
                 />
-                <input className='btn btn-primary' type='button' onClick={()=>props.handleSearchData()}
+                <input className='btn btn-primary' type='button' onClick={(e)=>props.handleSearchData(e)}
                  value="Search"/>
                 <i className='closeBtn' onClick={props.toggleSearch}/>
-            </div>
+            </form>
         </div>
         </Fragment>
     )
