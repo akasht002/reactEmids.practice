9a6f86be48d400f122565ccc647063c63ebabe65
abhisheksathyanarayana <abhisheksa@emids.com>
2019-02-20 21:58:28 +0530

    observation fixes

--- src/app/screens/Dashboard/serviceCalendar.js
+++ src/app/screens/Dashboard/serviceCalendar.js
@@ -42,7 +42,7 @@ class serviceCalendar extends React.Component {
       currentDate: moment().format("DD"),
       changedDate: "",
       DateDisable: false,
-      selectedServiceProviderId: "",
+      selectedServiceProviderId: null,
       DateLabelClass: "DatePickerDisabled",
       EditPersonalDetailModal: false,
       reportDay: moment(today).format(),
@@ -635,6 +635,7 @@ class serviceCalendar extends React.Component {
           centered="centered"
           headerFooter='d-none'
           btn1='Assign'
+          btn1Disable={!this.state.selectedServiceProviderId}
           btn2='Cancel'
           onConfirm={this.onSubmit}
           onCancel={() => {
