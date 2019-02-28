57050c90d14faafd8a9f03dee359a627eaff4717
abhisheksathyanarayana <abhisheksa@emids.com>
2019-02-28 18:49:04 +0530

    PR FIXES

--- src/app/screens/Profile/PersonalDetail/index.js
+++ src/app/screens/Profile/PersonalDetail/index.js
@@ -223,7 +223,6 @@ class PersonalDetail extends React.PureComponent {
   }
 
   closeImageUpload = () => {
-    this.resetImage();
     !this.isChangePhoto ? this.setState({
       uploadImage: !this.state.uploadImage,
       imageProfile: this.props.profileImgData.image,
