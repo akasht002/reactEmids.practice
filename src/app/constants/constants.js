7f7c369cad00c70533e3cac696919c41a4832a02
abhisheksathyanarayana <abhisheksa@emids.com>
2019-04-10 13:02:33 +0530

    HMUAT FIXES

--- src/app/constants/constants.js
+++ src/app/constants/constants.js
@@ -196,6 +196,8 @@ export const DEFAULT_VISIT_START_TIME = "0001-01-01T00:00:00"
 
 export const NO_PARTICIPANTS_FOUND = 'No additional participants can be added to this conversation.';
 
+export const NO_PARTICIPANTS_FOUND_CONFERENCE = 'No additional participants can be added to this conference.';
+
 export const NO_RESULT_FOUND = 'No result found';
 
 export const CONVERSATION_SUMMARY = 'conversationSummary';
