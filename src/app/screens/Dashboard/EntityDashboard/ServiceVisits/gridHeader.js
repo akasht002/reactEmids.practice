ca921092059b89f385aa9bdee3b3546a883314cd
rohitnagda <42795571+rohitnagda@users.noreply.github.com>
2019-09-10 13:42:31 +0530

    ui_fixes_demo

--- src/app/screens/Dashboard/EntityDashboard/ServiceVisits/gridHeader.js
+++ src/app/screens/Dashboard/EntityDashboard/ServiceVisits/gridHeader.js
@@ -1,47 +1,43 @@
 export let allServiceVisits = {
     servicePlanVisitNumber: 'SV ID',
-    serviceRequestNumber: 'SR ID',
     serviceCategoryDescription: 'Service Category',
     patientFullName: 'Individuals',
     visitStatus: 'Status',
     schedule: 'Schedule',
     task: 'Task',
-    providerFullName: 'Entity Service Provider',
+    providerFullName: 'Entity Provider',
     icon: 'Actions'
 }
 
 export let cancelledServiceVisits = {
     servicePlanVisitNumber: 'SV ID',
-    serviceRequestNumber: 'SR ID',
     serviceCategoryDescription: 'Service Category',
     patientFullName: 'Individuals',
     visitStatus: 'Status',
     schedule: 'Schedule',
     task: 'Task',
-    providerFullName: 'Entity Service Provider',
+    providerFullName: 'Entity Provider',
     icon: 'Actions'
 }
 
 export let lowTaskServiceVisits = {
     servicePlanVisitNumber: 'SV ID',
-    serviceRequestNumber: 'SR ID',
     serviceCategoryDescription: 'Service Category',
     patientFullName: 'Individuals',
     visitStatus: 'Status',
     schedule: 'Schedule',
     task: 'Task',
-    providerFullName: 'Entity Service Provider',
+    providerFullName: 'Entity Provider',
     icon: 'Actions'
 }
 
 export let overDueServiceVisits = {
     servicePlanVisitNumber: 'SV ID',
-    serviceRequestNumber: 'SR ID',
     serviceCategoryDescription: 'Service Category',
     patientFullName: 'Individuals',
     visitStatus: 'Status',
     schedule: 'Schedule',
     task: 'Task',
-    providerFullName: 'Entity Service Provider',
+    providerFullName: 'Entity Provider',
     icon: 'Actions'
 }
