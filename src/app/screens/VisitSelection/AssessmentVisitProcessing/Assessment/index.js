ce0a97e2408d88555db16a21f60e38ce20b27282
rohitnagda <42795571+rohitnagda@users.noreply.github.com>
2020-04-07 18:13:10 +0530

    ch12-342-ui-fixes-multi-chkbox

--- src/app/screens/VisitSelection/AssessmentVisitProcessing/Assessment/index.js
+++ src/app/screens/VisitSelection/AssessmentVisitProcessing/Assessment/index.js
@@ -377,7 +377,7 @@ export class Assessment extends Component {
                                                                         return ''
                                                                     })
                                                                     return (
-                                                                        <div className="form-check" key={answer.id}>
+                                                                        <div className="form-check pr-3" key={answer.id}>
                                                                             <label className='form-check-label'>
                                                                                 <input className="form-check-input"
                                                                                     id={answer.id}
