9877adc11682a6f52b2ea881f0a6521bccacb4af
Abhishek Sathyanarayana <AbhishekSa@emids.com>
2020-04-23 18:00:47 +0530

    pr fixes

--- src/app/data/AssessmentProcessingWizNavigationData.js
+++ src/app/data/AssessmentProcessingWizNavigationData.js
@@ -3,6 +3,7 @@ export const AssessmentProcessingWizNavigationData = [
         id: 0,
         title: 'Assessment',     
     },
+    // May required in future
     // {
     //     id: 1,
     //     title: 'Feedback',        
