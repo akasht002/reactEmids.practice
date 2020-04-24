1f0d18b99b32c6481cbcbb1ddccd04bb7c3070bb
rohitnagda <42795571+rohitnagda@users.noreply.github.com>
2020-04-24 15:06:21 +0530

    empty-ESP-dashboard-ui

--- src/app/screens/Dashboard/Components/CalendarDefault.js
+++ src/app/screens/Dashboard/Components/CalendarDefault.js
@@ -7,9 +7,18 @@ export const CalendarDefault = () => {
     <Fragment>  
         <div className='NoRequestServiceProvider '>  
         <div className='myPlanBlank theme-primary'><img src={require('../../../assets/images/my_plan_blank.png')} alt="my-plan-empty" /></div>
+        
+        {!getUserInfo().isEntityServiceProvider &&
         <span className="clickView theme-primary">Click below to get started.</span>
+        }
+        {getUserInfo().isEntityServiceProvider &&
+            <p>
+            <span className="text-center d-block  theme-primary pt-2">No Results</span>
+            <span className="text-center d-block p-3 default-444">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem </span>
+            </p>
+          }
             <div className="no-blockinfo-top">    
-          {!getUserInfo().isEntityServiceProvider && <Link className='NoServiceReq NewSPReq theme-primary-light' to='/Visitservicelist'>
+          {!getUserInfo().isEntityServiceProvider &&  <Link className='NoServiceReq NewSPReq theme-primary-light' to='/Visitservicelist'>
             <span className='NoInfoText'>
               <span className="NoServiceInfoLink theme-primary">
                 View Request
@@ -17,7 +26,9 @@ export const CalendarDefault = () => {
             </span>
           </Link>}         
             </div>
+
         </div>
     </Fragment>
   )
 }
+
